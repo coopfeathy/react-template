6d91f1e94782e8c6a83e379b0e38e6fcd88eedd2
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2019-12-11 22:37:13 -0600

    yes

--- src/App.js
+++ src/App.js
@@ -1,44 +1,14 @@
 import React, { Component } from "react"
 import "./index.css"
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Click for a joke from dear old dad."}</button>
-        <br/>
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
-
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Hey Daaaad... How'd you get in here???
+            This is a template for a React app
           </p>
-          <LambdaDemo />
         </header>
       </div>
     )
